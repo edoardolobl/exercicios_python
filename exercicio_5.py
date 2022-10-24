34237e16921ccdb3b408d6d0a203108aa47d10d2
edoardolobl <edoardo.lobl@gmail.com>
2022-10-24 20:26:13 -0300

    Versão final do exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -1,24 +1,51 @@
+def calculo_valor(quantidade_morango=0.0, quantidade_maca=0.0):
+
+    valor_total = 0
+    quantidade_total = quantidade_morango + quantidade_maca
+
+    if quantidade_morango <= 5:
+        preco_morango = 2.50 * quantidade_morango
+        valor_total = valor_total + preco_morango
+    else:
+        preco_morango = 2.20 * quantidade_morango
+        valor_total = valor_total + preco_morango
+
+    if quantidade_maca <= 5:
+        preco_maca = 1.8 * quantidade_maca
+        valor_total = valor_total + preco_maca
+    else:
+        preco_maca = 1.50 * quantidade_maca
+        valor_total = valor_total + preco_maca
+
+    if quantidade_total > 8 or valor_total > 25.0:
+        valor_total = valor_total - (valor_total * 0.1)
+        return round(valor_total, 2)
+    else:
+        return round(valor_total, 2)
+
+
+def quantidade_cliente(fruta):
+    quantidade = float(input(f"Qual a quantidade de {fruta} desejada? (em Kg)\n"))
+    return quantidade
+
+
 escolha_fruta = int(input("Qual fruta deseja comprar?\n(1) Morango\n(2) Maçã\n(3) Morango e Maçã\n"))
 
 if escolha_fruta < 1 or escolha_fruta > 3:
     raise Exception("Opção inválida")
 
-if escolha_fruta == 1 or escolha_fruta == 2:
-    quantidade_fruta = float(input("Qual a quantidade desejada? (em Kg)\n"))
+if escolha_fruta == 1:
+    quantidade_morango = quantidade_cliente("morango")
+    valor = calculo_valor(quantidade_morango, 0.0)
+    print(f"{quantidade_morango}Kg de morango por R${valor}")
+elif escolha_fruta == 2:
+    quantidade_maca = quantidade_cliente("maçã")
+    valor = calculo_valor(0.0, quantidade_maca)
+    print(f"{quantidade_maca}Kg de morango por R${valor}")
 else:
-    quantidade_morango = float(input("Qual a quantidade de morango desejada? (em Kg)\n"))
-    quantidade_maca = float(input("Qual a quantidade de maçã desejada? (em Kg)\n"))
+    quantidade_morango = quantidade_cliente("morango")
+    quantidade_maca = quantidade_cliente("maçã")
+    valor = calculo_valor(quantidade_morango, quantidade_maca)
+    print(f"{quantidade_morango}Kg de morango e {quantidade_maca}Kg de maçã por R${valor}")
 
 
-def calculo_valor(quantidade)
-
-if quantidade < 5:
-    preco_morango = 2.50 * quantidade
-    preco_maca = 1.8 * quantidade
-
-elif quantidade > 5 and quantidade <= 8:
-    preco_morango = 2.20 * quantidade
-    preco_maca = 1.50 * quantidade
-
-if quantidade > 8 or valor_total > 25.0:
-    valor_total = valor_total * 0.1
